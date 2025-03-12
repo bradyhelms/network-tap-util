81968f60dc4043af158c3fae9c4b8bb06b933ecf
Brady Helms <brady.helms@i-techsupport.com>
2025-03-12 14:20:16 -0400

    add pcap file transfer

--- src/capture/capture.go
+++ src/capture/capture.go
@@ -6,6 +6,7 @@ import (
 	"github.com/bradyhelms/network-tap-util/src/utils"
 	"log"
 	"os"
+	"time"
 )
 
 // First connect to board over ssh
@@ -21,10 +22,28 @@ func main() {
 	}
 	defer tapClient.Close()
 
-	if yesNo("Start packet capture?") {
-		err = utils.RunCommand(tapClient, "/home/petalinux/scripts/tap")
+	if !yesNo("Start packet capture?") {
+		os.Exit(0)
 	}
 
+	err = utils.RunCommand(tapClient, "/home/petalinux/scripts/tap 60")
+	if err != nil {
+		log.Printf("Error running command: %s", err)
+	}
+
+	fmt.Println("Running packet capture for 60 seconds.")
+	fmt.Println("Please wait")
+	for i := 1; i < 13; i++ {
+		time.Sleep(5 * time.Second)
+		fmt.Printf(".")
+	}
+	fmt.Printf("\nDone!\n")
+
+	fmt.Println("Starting .pcap file transfer.")
+	err = utils.RunCommand(tapClient, "scp /home/petalinux/scripts/capture.pcap petalinux@192.168.1.209:/home/petalinux/captures")
+	if err != nil {
+		log.Printf("Error receiving file: %s", err)
+	}
 }
 
 func yesNo(question string) bool {
