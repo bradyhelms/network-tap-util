80f33b1f4e579079b929d788083ebcdd695f56e1
Brady Helms <brady.helms@i-techsupport.com>
2025-03-12 11:52:34 -0400

    add key-based auth

--- src/main.go
+++ src/main.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-//  "bufio"
-  "fmt"
-  "log"
-  "time"
-  "github.com/bradyhelms/network-tap-util/src/utils"
+	//  "bufio"
+	"fmt"
+	"github.com/bradyhelms/network-tap-util/src/utils"
+	"log"
+	"time"
 )
 
 func main() {
-  // Get BERTHA credentials
-  berthaIP := "172.13.201.12"
-  fmt.Println("Connecting to BERTHA.")
+	// Get BERTHA credentials
+	berthaIP := "172.13.201.12"
+	fmt.Println("Connecting to BERTHA.")
 
-  berthaClient, err := utils.GetSshClient(berthaIP)
-  if err != nil {
-    log.Fatalf("Error establishing ssh connection: %s", err)
-  } else {
-    fmt.Println("Conenction with BERTHA established.")
-  }
-  defer berthaClient.Close()
-  
-  fmt.Println("Starting the Vivado hardware server on BERTHA.")
-  err = utils.RunCommand(berthaClient, "nohup /tools/Xilinx/Vivado/2024.1/bin/hw_server > /dev/null 2>&1 & exit")
-  if err != nil {
-    log.Printf("Error running command: %s", err)
-  }
+	berthaClient, err := utils.GetSshClient(berthaIP)
+	if err != nil {
+		log.Fatalf("Error establishing ssh connection: %s", err)
+	} else {
+		fmt.Println("Conenction with BERTHA established.")
+	}
+	defer berthaClient.Close()
 
-  // Wait a bit before trying to program the board
-  time.Sleep(5 * time.Second)
+	fmt.Println("Starting the Vivado hardware server on BERTHA.")
+	err = utils.RunCommand(berthaClient, "nohup /tools/Xilinx/Vivado/2024.1/bin/hw_server > /dev/null 2>&1 & exit")
+	if err != nil {
+		log.Printf("Error running command: %s", err)
+	}
 
-  // Program the FPGA board
-  almaIP := "192.168.50.54"
-  fmt.Println("Connecting to Alma.")
+	// Wait a bit before trying to program the board
+	time.Sleep(5 * time.Second)
 
-  almaClient, err := utils.GetSshClient(almaIP)
-  if err != nil {
-    log.Fatalf("Error establishing ssh connection: %s", err)
-  } else {
-    fmt.Println("Conenction with Alma established.")
-  }
-  defer almaClient.Close()
+	// Program the FPGA board
+	almaIP := "192.168.50.54"
+	fmt.Println("Connecting to Alma.")
 
-  fmt.Println("Progamming the FPGA board.")
-  err = utils.RunCommand(almaClient, "source /opt/PetaLinux/settings.sh" +
-                               "&& cd /opt/bsp_build_20250310/xilinx-sp701-2024.1/images/linux/" + 
-                               "&& /opt/PetaLinux/scripts/petalinux-boot jtag --prebuilt 3 " +
-                               "--hw_server-url 172.13.201.12:3121")
-  if err != nil {
-    log.Printf("Error running command: %s", err)
-  }
+	almaClient, err := utils.GetSshClient(almaIP)
+	if err != nil {
+		log.Fatalf("Error establishing ssh connection: %s", err)
+	} else {
+		fmt.Println("Conenction with Alma established.")
+	}
+	defer almaClient.Close()
+
+	fmt.Println("Progamming the FPGA board.")
+	err = utils.RunCommand(almaClient, "source /opt/PetaLinux/settings.sh"+
+		"&& cd /opt/bsp_build_20250310/xilinx-sp701-2024.1/images/linux/"+
+		"&& /opt/PetaLinux/scripts/petalinux-boot jtag --prebuilt 3 "+
+		"--hw_server-url 172.13.201.12:3121")
+	if err != nil {
+		log.Printf("Error running command: %s", err)
+	}
 }
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,65 +1,80 @@
 package utils
 
 import (
-  "fmt"
-  "os"
-  "golang.org/x/crypto/ssh"
-  "golang.org/x/term"
-  "log"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/term"
+	"log"
+	"os"
 )
 
 type SshCreds struct {
-  User string
-  Pass string
+	User string
+	Pass string
 }
 
-func GetSshCredentials(hostname string) (creds SshCreds){
-  fmt.Printf("Enter ssh credentials for %s.\n", hostname)
+func GetSshCredentials(hostname string) (creds SshCreds) {
+	fmt.Printf("Enter ssh credentials for %s.\n", hostname)
 
-  fmt.Printf("Username: ")
-  fmt.Scanln(&creds.User)
+	fmt.Printf("Username: ")
+	fmt.Scanln(&creds.User)
 
-  fmt.Printf("Password: ")
-  tempPass, err := term.ReadPassword(int(os.Stdin.Fd()))
-  if err != nil {
-    log.Fatalf("Failed to read password: %s", err)
-  }
+	fmt.Printf("Password: ")
+	tempPass, err := term.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		log.Fatalf("Failed to read password: %s", err)
+	}
 
-  creds.Pass = string(tempPass)
-  fmt.Println()
-  return creds 
+	creds.Pass = string(tempPass)
+	fmt.Println()
+	return creds
 }
 
 func GetSshClient(ip string) (*ssh.Client, error) {
-  creds := GetSshCredentials(ip)
+	creds := GetSshCredentials(ip)
 
-  clientConf := &ssh.ClientConfig{
-    User: creds.User,
-    Auth: []ssh.AuthMethod{
-      ssh.Password(creds.Pass),
-    },
-    HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-  }
+	clientConf := &ssh.ClientConfig{
+		User: creds.User,
+		Auth: []ssh.AuthMethod{
+			publicKeyAuth(),
+			ssh.Password(creds.Pass),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
 
-  client, err := ssh.Dial("tcp", ip+":22", clientConf)
-  if err != nil {
-    return nil, fmt.Errorf("Failed to dial: %w", err)
-  }
+	client, err := ssh.Dial("tcp", ip+":22", clientConf)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to dial: %w", err)
+	}
 
-  return client, nil
+	return client, nil
 }
 
 func RunCommand(client *ssh.Client, cmd string) error {
-  session, err := client.NewSession()
-  if err != nil {
-    return fmt.Errorf("Failed to create session: %w", err)
-  }
-  defer session.Close()
+	session, err := client.NewSession()
+	if err != nil {
+		return fmt.Errorf("Failed to create session: %w", err)
+	}
+	defer session.Close()
 
-  output, err := session.CombinedOutput(cmd)
-  if err != nil {
-    return fmt.Errorf("Failed to run command: %w.\n\nOutput:\n%s", err, output)
-  }
-  fmt.Printf("Output of command '%s':\n%s\n", cmd, output)
-  return nil
+	output, err := session.CombinedOutput(cmd)
+	if err != nil {
+		return fmt.Errorf("Failed to run command: %w.\n\nOutput:\n%s", err, output)
+	}
+	fmt.Printf("Output of command '%s':\n%s\n", cmd, output)
+	return nil
+}
+
+func publicKeyAuth() ssh.AuthMethod {
+	key, err := os.ReadFile("/path/to/id_rsa")
+	if err != nil {
+		log.Fatal("Failed to read private key: ", err)
+	}
+
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		log.Fatal("Failed to parse private key: ", err)
+	}
+
+	return ssh.PublicKeys(signer)
 }
